463485dfc31e688c32339be697da23f3a00d9f4d
Aurelius Trisandio Prasetyo <aurelius.lt2003@gmail.com>
2023-10-28 09:39:20 +0700

    Summary

---
+++ Lawang_sewu/Assets/Script/Player2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player2 : MonoBehaviour
+{
+    public Button Left;
+    public Button Mid;
+    public Button Right;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
