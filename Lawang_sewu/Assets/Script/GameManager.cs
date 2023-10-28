6339819a3fd6ea833144aa29526a62aa6527856e
Aurelius Trisandio Prasetyo <aurelius.lt2003@gmail.com>
2023-10-28 22:06:05 +0700

    Summary

--- Lawang_sewu/Assets/Script/GameManager.cs
+++ Lawang_sewu/Assets/Script/GameManager.cs
@@ -8,30 +8,53 @@ public class GameManager : MonoBehaviour
 {
     public Button start;
     public Button quit;
+    public Button p1;
+    public Button p2;
+
+    public Canvas chose;
     void Start()
     {
-        start.onClick.AddListener(GameScene);
+        start.onClick.AddListener(Chose);
         quit.onClick.AddListener(QuitGame);
-    }
 
-    void GameScene()
+        p1.onClick.AddListener(P1);
+        p2.onClick.AddListener(P2);
+    }
+    void P1()
     {
-        // wait 1 seconds
-        StartCoroutine(LoadGameScene());
+        StartCoroutine(LoadP1());
+    }
+    void P2()
+    {
+        StartCoroutine(LoadP2());
     }
     void QuitGame()
     {
        StartCoroutine(LoadQuitGame());
     }
 
-    // 2 Second Timer before load GameScene
-    IEnumerator LoadGameScene(){
+    void Chose()
+    {
+        StartCoroutine(LoadChose());
+    }
+
+    IEnumerator LoadP1(){
         yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("Level2");
     }
-    
+    IEnumerator LoadP2()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Level3");
+    }
+
     IEnumerator LoadQuitGame(){
         yield return new WaitForSeconds(1f);
         Application.Quit();
+    }
+    IEnumerator LoadChose()
+    {
+        yield return new WaitForSeconds(1f);
+        chose.enabled = true;
     }
 }
